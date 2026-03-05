cad22df434c53cf2e66d977423490d5c1e3e7589
DannyM2027 <daniel.matut2027@gbwl.org>
2026-03-05 19:22:34 +0000

    4..11

--- Main.java
+++ Main.java
@@ -1,6 +1,56 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // 2D array fata structure
+      // like an ARRAY but w rows and columns like grid
+      // rows horizantal and columns vert
+
+      // declare a 2d array w double sq brackets
+      int[][] experimentData;
+      double[][] pointValues;
+
+      expirementData = new int[10][3]; // 10 row 3 col
+
+      // initilize 2 d aray
+      String[][] seatingChart = new String[4][6];
+      // 4 x 6
+
+      // set a value by accessing a cell
+      // acess a cell: arrayName[row][col]
+      seatingChart[0][0] = "Mia";   
+      seatingChart[0][5] = "sohaila";   
+      seatingChart[1][0] = "Ethan";
+      seatingChart[1][5] = "Z";   
+      seatingChart[1][1] = "AA";   
+      seatingChart[2][3] = "Sim";
+      seatingChart[2][4] = "Bo";
+      seatingChart[3][0] = "shwartz";
+      seatingChart[2][3] = "Sim";
+      seatingChart[2][3] = "Sim";
+      seatingChart[2][3] = "Sim";
+      seatingChart[2][3] = "Sim";
+      seatingChart[2][3] = "Sim";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    }
 }
